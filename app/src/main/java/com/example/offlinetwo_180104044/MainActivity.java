48fbc36a8a0b1df690133d5e55689e1a9f797cbe
pranto1416 <pranto1416@gmail.com>
2020-03-20 00:09:50 +0600

    offline 2

---
+++ app/src/main/java/com/example/offlinetwo_180104044/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.offlinetwo_180104044;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
